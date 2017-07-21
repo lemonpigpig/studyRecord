0009d8d71ce6170d9ff90462bc58ccc4f78b16cb
hexianjun <hexianjuan@gznb.com>
2017-07-21 12:02:10 +0800

    vue

---
+++ vuemark.vue
@@ -0,0 +1,13 @@
+<script>
+// 影响性能
+this.firstCategory.forEach((data) => {
+  if (data.code === code) {
+    data.currenticon = data.icon === '' ? '/img/category-icon.svg' : data.icon
+  }
+})
+// good solve way
+this.$set(this, 'firstCategory', this.firstCategory.map(data => Object.assign({}, data, {
+  currenticon: data.icon === '' ? '/img/category-icon.svg' : data.icon
+})))
+
+</script>
