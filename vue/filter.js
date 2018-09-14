17d2e9798b54ab75742f6aab49eb088c56296524
贺贤娟 <lemon.he@boqii.com>
2018-09-14 15:47:53 +0800

    feat: add filter for time

---
+++ vue/filter.js
@@ -0,0 +1,12 @@
+﻿Vue.filter( 'time-filter', (input)=>{
+    if( !input ) return;
+    let datetime = new Date(input*1000);
+    let year = datetime.getFullYear();
+    let month = datetime.getMonth() + 1;
+    let date = datetime.getDate();
+    let hour = datetime.getHours();
+    let minute = datetime.getMinutes();
+    let second = datetime.getSeconds();
+    let mseconds = datetime.getMilliseconds();
+    return year + "-" + month + "-" + date;
+})
