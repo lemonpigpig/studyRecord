fbb9efae58a48e63eff49d0cb2ab78eab18c504d
贺贤娟 <lemon.he@boqii.com>
2018-12-21 17:20:43 +0800

    feat:add

---
+++ babel/src/clazz.js
@@ -0,0 +1,5 @@
+class Parent {
+  then() {
+    console.log('--func---')
+  }
+}
--- js/动态规划问题/d.js
+++ js/动态规划问题/d.js
@@ -1,48 +1,47 @@
 function minCoin(values, valueKinds, money, coinsUsed) {
-  coinsUsed[0] = 0;
+  coinsUsed[0] = 0
   for (let cents = 1; cents <= money; cents++) {
-    let minCoins = cents;
+    let minCoins = cents
     for (let kind = 0; kind < valueKinds; kind++) {
       // 若当前面值的硬币小于当前的cents则分解问题并查表
       if (values[kind] <= cents) {
-        let temp = coinsUsed[cents - values[kind]] + 1;
+        let temp = coinsUsed[cents - values[kind]] + 1
         if (temp < minCoins) {
-          minCoins = temp;
+          minCoins = temp
         }
       }
     }
-    coinsUsed[cents] = minCoins;
+    coinsUsed[cents] = minCoins
     //   console.log("面值为 " + (cents) + " 的最小硬币数 : " + coinsUsed[cents])
   }
   return coinsUsed
 }
 
 function minCoin1(values, valueKinds, money, coinsUsed, min) {
-  coinsUsed[0] = 0;
+  coinsUsed[0] = 0
   for (let cents = 1; cents <= money; cents++) {
-    let minCoins = cents;
+    let minCoins = cents
     for (let kind = 0; kind < valueKinds; kind++) {
       // 若当前面值的硬币小于当前的cents则分解问题并查表
       if (values[kind] <= cents) {
-        let temp = coinsUsed[cents - values[kind]] + min;
-        if(temp === money) {
+        let temp = coinsUsed[cents - values[kind]] + min
+        if (temp === money) {
           if (temp < minCoins) {
-            minCoins = temp;
+            minCoins = temp
           }
         }
-       
       }
     }
-    if(minCoins === cents) {
+    if (minCoins === cents) {
       minCoins = 0
     }
-    coinsUsed[cents] = minCoins;
+    coinsUsed[cents] = minCoins
     //   console.log("面值为 " + (cents) + " 的最小硬币数 : " + coinsUsed[cents])
   }
   return coinsUsed
 }
-var coiValue = [25, 21, 10, 5, 1];
-minCoin(coiValue, coiValue.length, 65, []);
+var coiValue = [25, 21, 10, 5, 1]
+minCoin(coiValue, coiValue.length, 65, [])
 
-var coiValue1 = [25, 21, 10, 5, 3];
-minCoin1(coiValue1, coiValue1.length, 2, [], 3);
+var coiValue1 = [25, 21, 10, 5, 3]
+minCoin1(coiValue1, coiValue1.length, 2, [], 3)
